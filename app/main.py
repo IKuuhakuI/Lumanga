0a2d2569ff3cb3bb4fd94543293e29ff94cd3eca
Luiz Carlos <luizcarlosaguiarc@gmail.com>
2020-10-10 02:31:00 -0300

    cria sessoes temporarias

--- app/main.py
+++ app/main.py
@@ -1,26 +1,37 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
+from datetime import timedate
 
 app = Flask(__name__)
-app.secret_key = "hello"
+app.secret_key = "teste"
+app.permanent_session_lifetime = timedelta(minutes=5)
 
 @app.route ("/")
 def inicial ():
-	flash ("O jogo")
 	return render_template ("inicio.html")
 
 @app.route("/entrar", methods=["POST", "GET"])
 def login():
 	if request.method == "POST":
-		user = request.form ["email"]
+		session.permanent = True
+		userEmail = request.form ["email"]
+		session["email"] = userEmail
 
-		return redirect (url_for ("user", usr=user))
+		return redirect (url_for ("user"))
 
-	else:	
+	else:
+		if "email" in session:
+			return redirect(url_for("user"))
 		return render_template ("entrar.html")
 
-@app.route("/<usr>")
-def user (usr):
-	return f"<h1>{usr}</h1>"
+@app.route("/user")
+def user ():
+	if "email" in session:
+		email = session["email"]
+
+		return f"<h1>{email}</h1>"
+
+	else:
+		return redirect (url_for ("login"))
 
 @app.route("/consultas")
 def consultas():
@@ -29,6 +40,12 @@ def consultas():
 @app.route("/eventos")
 def eventos():
 	return render_template ("eventos.html")
+
+
+@app.route("/sair")
+def logout():
+	session.pop("email", None)
+	return redirect( url_for ("inicial"))
 
 if __name__ == "__main__":
 	app.run (debug=True)
