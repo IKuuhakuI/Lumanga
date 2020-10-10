48ebcda8634bb242f5aaca6ca593cc1a236bbf1a
Matheus Pereira Kulick <matheuskulick@hotmail.com>
2020-10-09 23:51:27 -0300

    add login page

--- app/main.py
+++ app/main.py
@@ -6,5 +6,18 @@ app = Flask(__name__)
 def inicial ():
 	return render_template ("inicio.html")
 
+@app.route("/entrar")
+def login():
+	return render_template ("entrar.html")
+
+@app.route("/consultas")
+def consultas():
+	return render_template ("consultas.html")
+
+@app.route("/eventos")
+def eventos():
+	return render_template ("eventos.html")
+
+
 if __name__ == "__main__":
 	app.run (debug=True)
