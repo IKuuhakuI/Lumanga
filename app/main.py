e28af920c19d1f061d4945c27a070546b0076139
Luiz Carlos <luizcarlosaguiarc@gmail.com>
2020-10-10 02:10:59 -0300

    utiliza post para pegar email do user

--- app/main.py
+++ app/main.py
@@ -1,14 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
+app.secret_key = "hello"
 
 @app.route ("/")
 def inicial ():
+	flash ("O jogo")
 	return render_template ("inicio.html")
 
-@app.route("/entrar")
+@app.route("/entrar", methods=["POST", "GET"])
 def login():
-	return render_template ("entrar.html")
+	if request.method == "POST":
+		user = request.form ["email"]
+
+		return redirect (url_for ("user", usr=user))
+
+	else:	
+		return render_template ("entrar.html")
+
+@app.route("/<usr>")
+def user (usr):
+	return f"<h1>{usr}</h1>"
 
 @app.route("/consultas")
 def consultas():
@@ -17,7 +29,6 @@ def consultas():
 @app.route("/eventos")
 def eventos():
 	return render_template ("eventos.html")
-
 
 if __name__ == "__main__":
 	app.run (debug=True)
