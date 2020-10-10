5e9f28b78cae771a933328d3bac8e9d31e8ffcbc
Luiz Carlos <luizcarlosaguiarc@gmail.com>
2020-10-10 03:32:34 -0300

    faz o menu mostrar so os botoes ativos

--- app/main.py
+++ app/main.py
@@ -5,9 +5,14 @@ app = Flask(__name__)
 app.secret_key = "teste"
 app.permanent_session_lifetime = timedelta(minutes=5)
 
+def checarTipoLogin():
+	if (session["logged_in"]):
+		return True
+	return False
+
 @app.route ("/")
 def inicial ():
-	return render_template ("inicio.html")
+	return render_template ("inicio.html", logado=checarTipoLogin())
 
 @app.route("/entrar", methods=["POST", "GET"])
 def entrar():
@@ -16,30 +21,35 @@ def entrar():
 		userEmail = request.form ["email"]
 		session["email"] = userEmail
 
+		flash ("Entrou", "info")
+
 		return redirect (url_for ("user"))
 
 	else:
 		if "email" in session:
+			flash ("Ja esta logado")
 			return redirect(url_for("user"))
-		return render_template ("entrar.html")
+		return render_template ("entrar.html", logado=checarTipoLogin())
 
 @app.route("/user")
 def user ():
 	if "email" in session:
+		logado = True
 		email = session["email"]
+		session['logged_in'] = True
 
-		return f"<h1>{email}</h1>"
+		return render_template("perfil.html", user=email, logado=checarTipoLogin())
 
 	else:
 		return redirect (url_for ("entrar"))
 
 @app.route("/consultas")
 def consultas():
-	return render_template ("consultas.html")
+	return render_template ("consultas.html", logado=checarTipoLogin())
 
 @app.route("/eventos")
 def eventos():
-	return render_template ("eventos.html")
+	return render_template ("eventos.html", logado=checarTipoLogin())
 
 
 @app.route("/sair")
@@ -47,11 +57,12 @@ def logout():
 	if "email" in session:
 		user = session["email"]
 		flash (f"{user}, saiu", "info")
+	
+	session['logged_in']=False
 	session.pop("email", None)
 
-	
 
-	return redirect( url_for ("entrar"))
+	return redirect( url_for ("inicial"))
 
 if __name__ == "__main__":
 	app.run (debug=True)
