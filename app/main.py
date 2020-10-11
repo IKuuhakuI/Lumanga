ca60ef668395a9f010728e83070229d1b9a2ac5e
Luiz Carlos <luizcarlosaguiarc@gmail.com>
2020-10-11 12:31:12 -0300

    comenta os codigos do back-end

--- app/main.py
+++ app/main.py
@@ -2,17 +2,22 @@ from flask import Flask, render_template, request, redirect, url_for, session, f
 from datetime import timedelta
 from flask_sqlalchemy import SQLAlchemy
 
+# Inicia o app
 app = Flask(__name__)
 app.secret_key = "teste"
 
+# Conexao com o banco de dados
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+# Deixa o User conectado por 30 minutos
 app.permanent_session_lifetime = timedelta(minutes=30)
 
 db = SQLAlchemy(app)
 
+# Classe para usuario do db
 class Users (db.Model):
+	# Colunas
 	_id = db.Column (db.Integer, primary_key=True)
 	name = db.Column ("name", db.String (100))
 	email = db.Column ("email", db.String (100), unique=True)
@@ -25,6 +30,7 @@ class Users (db.Model):
 
 db.create_all()
 
+# Checa se o user esta logado
 def checarTipoLogin():
 	if (session.get("logged_in") == None):
 		return False
@@ -33,8 +39,10 @@ def checarTipoLogin():
 		return True
 	return False
 
+# Pagina Inicial
 @app.route ("/")
 def inicial ():
+	# Caso logado
 	if "email" in session:
 		logado = True
 		email = session["email"]
@@ -42,6 +50,7 @@ def inicial ():
 
 	return render_template ("inicio.html", logado=checarTipoLogin())
 
+# Pagina de Login
 @app.route("/entrar", methods=["POST", "GET"])
 def entrar():
 	if request.method == "POST":
@@ -51,7 +60,9 @@ def entrar():
 
 		usuario = Users.query.filter_by(email=userEmail).first()
 
+		# Caso usuario exista
 		if (usuario):
+			# Senha valida
 			if usuario.password == userPass:
 				session.permanent = True
 				session["email"] = userEmail
@@ -61,6 +72,7 @@ def entrar():
 			else:
 				flash ("Senha incorreta", "warning")
 
+		# Caso nao exista
 		else:
 			flash ("User nao existe", "warning")
 
@@ -68,60 +80,74 @@ def entrar():
 		
 
 	else:
+		# Caso ja esteja logado
 		if "email" in session:
 			flash ("Ja esta logado")
 			return redirect(url_for("user"))
 		return render_template ("entrar.html", logado=checarTipoLogin())
 
+# Pagina de usuario
 @app.route("/user")
 def user ():
 	flash ("Consulta 'X' pendente!")
 	return render_template("perfil.html", logado=checarTipoLogin())
 
+# Pagina de consultas
 @app.route("/consultas")
 def consultas():
 	return render_template ("consultas.html", logado=checarTipoLogin())
 
+# Pagina de eventos
 @app.route("/eventos")
 def eventos():
 	return render_template ("eventos.html", logado=checarTipoLogin())
 
+# Pagina de videos
 @app.route("/videos")
 def videos():
 	return render_template ("videos.html", logado=checarTipoLogin())
 
+# Pagina agendar
 @app.route("/agendar")
 def agendar():
 	return render_template ("agendar.html", logado=checarTipoLogin())
 
+# Sair do usuario
 @app.route("/sair")
 def logout():
+	# Caso esteja logado
 	if "email" in session:
 		user = session["email"]
 		flash (f"{user}, saiu", "info")
 	
+	# Desconecta o usuario
 	session['logged_in']=False
 	session.pop("email", None)
 
 
 	return redirect( url_for ("inicial"))
 
+# Pagina registrar
 @app.route("/registrar", methods=["POST", "GET"])
 def registrar():
 	valNome = ""
 
 	if request.method == "POST":
+		# Dados do User
 		dataName = request.form.get('name', valNome)
 		dataEmail = request.form.get('email', valNome)
 		dataPass = request.form.get('password', valNome)
 		dataConfirm = request.form.get('confirmPassword', valNome)
 		
+		# Caso senha e confirmacao sejam iguais
 		if dataPass == dataConfirm:
+			# Verifica se o email ja existe
 			if Users.query.filter_by(email=dataEmail).first():
 				flash ("email ja cadastrado")
 				return redirect (url_for ("registrar"))
 
 			else:
+				# Cria o novo user
 				usuario = Users (dataName, dataEmail, dataPass)
 
 				db.session.add(usuario)
@@ -141,6 +167,7 @@ def registrar():
 
 	return render_template ("registrar.html", logado=checarTipoLogin())
 
+# PROPOSITO DE DEBUG APENAS (VER TODOS OS USERS E SENHAS)
 @app.route ("/view")
 def view ():
 	return render_template ("view.html", values=Users.query.all())
